4a617d75abda6d117187bfcca28197812830af0f
Amrit Sundarka <amritsundarka@gmail.com>
2024-03-07 07:01:17 +0530

    Intital Coomit with Working

---
+++ mathracer/src/Components/header.js
@@ -0,0 +1,19 @@
+// Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="www">Home</a></li>
+          <li><a href="www">About</a></li>
+          <li><a href="www">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ mathracer/src/Components/homePage.js
@@ -0,0 +1,19 @@
+// HomePage.js
+
+import React from 'react';
+import Header from './header';
+import Footer from './footer';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <Header />
+      <main>
+        {/* Content of the homepage */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default HomePage;
